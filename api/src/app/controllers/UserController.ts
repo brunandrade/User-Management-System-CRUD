33fff985ad2c974b5e0f1c92cec267e1edb02a5f
Bruna Andrade <bruna.card.andrade@gmail.com>
2020-12-18 16:19:54 -0300

    Rota para alteração de senha do usuário

--- api/src/app/controllers/UserController.ts
+++ api/src/app/controllers/UserController.ts
@@ -197,8 +197,29 @@ class UserController{
     else{
         return res.status(201).json(user); 
     }
-  }
-          
+  }     
+  
+  async changePassword(req : Request, res: Response){
+    const repository = getRepository(User);
+    const {Email, Password} = req.body
+    const user = await repository.findOne( { where: { Email  } } ); 
+
+    if(!user){
+      return res.status(400).send({
+        Success: false,
+        Message: "Usuário não encontrado!"               
+      });
+    }
+
+    const userResult = repository.merge(user, {Password});
+    await repository.save(userResult);
+    
+  return res.status(201).send({
+    Success: true,
+    Message: "Senha atualizada com sucesso!",
+    user: userResult
+  });
+}
 }
 
 export default new UserController();
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -9,6 +9,8 @@ router.post('/User/Create', UserController.store)
 
 router.post('/User/Login', UserController.login)
 
+router.put('/User/ChangePassword', UserController.changePassword)
+
 router.put('/User/:Id/Update', authMiddleware, UserController.update)
 
 router.delete('/User/:Id/Remove', authMiddleware, UserController.remove)
@@ -16,6 +18,8 @@ router.delete('/User/:Id/Remove', authMiddleware, UserController.remove)
 router.get('/User/List', authMiddleware, UserController.index)
 
 router.get('/User/:Id/Detail', authMiddleware, UserController.detail)
+
+
 
 //HeadOffice
 router.post('/HeadOffice/Create', authMiddleware, HeadOfficeController.store)
